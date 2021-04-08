43147e1873498bbbc188da8c0738ec41e5deae35
Васильев Виталий <vasilev.v.vitalii@gmail.com>
2021-04-08 15:51:02 +0300

    export exec_result_message

--- index.js
+++ index.js
@@ -22,6 +22,9 @@ exports.options_beautify = options_beautify
  * @typedef {type.exec_result_table} exec_result_table
  */
 /**
+ * @typedef {type.exec_result_message} exec_result_message
+ */
+/**
  * @typedef {type.exec_lock} exec_lock
  */
 /**
